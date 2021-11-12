c6b8ecc41eb9b5dcf79eb83186e0a934ec9ad885
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:50:33 -0500

    serializer prescription

--- app/serializers/prescription_serializer.rb
+++ app/serializers/prescription_serializer.rb
@@ -1,3 +1,7 @@
 class PrescriptionSerializer < ActiveModel::Serializer
   attributes :id, :daily_dosage, :directions, :doses_in_container, :doctor_id, :medication_id, :user_id
+  
+  has_one :medication
+  has_one :doctor
+  belongs_to :user
 end

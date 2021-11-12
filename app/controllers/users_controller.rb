0fce7847a4911ee7a1d6338ad10e980a8c9f8718
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:44:50 -0500

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: [:create, :show]
+
+    def index
+        users=User.all
+        render json: users, include: :prescriptions
+    end
+    
+    def create
+        user=User.create(user_params)
+        session[:user_id]=user.id
+        if user.valid?
+            render json:user, status: :created, include: :prescriptions
+        else
+            render json: {error:user.errors.full_messages}
+        end
+    end
+
+    def show
+        usershow=User.find_by(id:params[:id])
+        render json: usershow, include: :prescriptions
+    end
+        
+    private
+
+    def user_params
+        params.require(:user).permit(:user_name,:password)
+    end
 end

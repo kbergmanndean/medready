72c18929a3ce87a224280222e74ae09c3829ce7a
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:42:28 -0500

    medications controller

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,13 @@
 class MedicationsController < ApplicationController
+    skip_before_action :authorize
+
+    def index
+        meds=Medication.all
+        render json: meds
+    end
+
+    def show
+        med=Medication.find_by(id:params[:id])
+        render json: med
+    end
 end

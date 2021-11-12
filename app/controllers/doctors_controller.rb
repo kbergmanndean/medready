1fc4d31015f969bfad0e8d7c03f305d04c5e1cd3
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:41:22 -0500

    doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,35 @@
 class DoctorsController < ApplicationController
+    skip_before_action :authorize
+    def index
+        doctors=Doctor.all
+        render json: doctors
+    end
+
+    def show
+        doctor=Doctor.find_by(id:params[:id])
+        render json: doctor
+    end
+
+    def create
+        new_doctor=Doctor.create(doctor_params)
+        if new_doctor.valid?
+            render json: new_doctor, status: :created
+        else
+            render json: {error:new_doctor.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        delete_doc=Doctor.find_by(id:params[:id])
+        delete_doc.destroy
+        head:no_content
+    end
+
+    private
+
+    def doctor_params
+        params.permit(:name, :profession)
+    end
+
+
 end

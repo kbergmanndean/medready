798681675c3bf6ce52a4ca8e35037583eed85678
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:47:46 -0500

    model for doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,7 @@
 class Doctor < ApplicationRecord
+    has_many :prescriptions, dependent: :destroy
+    has_many :medications,through: :prescriptions 
+
+    validates :name, presence: true
+    validates :profession, presence: true
 end

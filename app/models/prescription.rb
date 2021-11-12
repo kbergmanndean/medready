725f038f0a44b98368e32bf8de6af42ac8c12c3f
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:48:46 -0500

    prescription model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,2 +1,11 @@
 class Prescription < ApplicationRecord
+    belongs_to :doctor
+    belongs_to :medication
+    belongs_to :user
+
+    validates :daily_dosage, presence: true, numericality: true
+    validates :directions, presence: true 
+    validates :doses_in_container, presence: true, numericality: {only_integer: true}
+    validates :doctor_id, presence: true 
+    validates :medication_id, presence: true
 end

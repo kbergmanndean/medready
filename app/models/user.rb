4e6d47a36384f5732764d8e648211d9a3abf10fa
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:49:16 -0500

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    # has_many :doctors
+    has_many :medications, through: :prescriptions
+    has_many :prescriptions
+    
+    has_secure_password
+
+    validates :user_name, presence:{message: "invalid username and/or password"}, uniqueness:true 
+    validates :password, presence:true
 end

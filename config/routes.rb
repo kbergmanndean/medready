3208c52eba4beaa39e26b93b094d71cb1b49cc0f
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 18:56:43 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :users
+  resources :users, only:[:show,:create]
   resources :prescriptions
-  resources :medications
-  resources :doctors
+  resources :medications, only: [:index,:show]
+  resources :doctors, only: [:index,:show,:create,:destroy]
+  post "/log_in", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

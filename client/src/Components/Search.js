562028ecae0291a5f1f1a5f23a57d9c95ff87294
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 20:37:27 -0500

    search component

---
+++ client/src/Components/Search.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import LoginAuth from './LoginAuth'
+
+function Search({setMed, meds, user, setUser}){
+    const [search, setSearch]=useState("")
+
+    const handleSearch=(e)=>{setSearch(e.target.value)}
+    const displayMeds=meds.length>0? meds.filter(m=>m.generic_name.toLowerCase().includes(search.toLowerCase())):meds
+
+   
+
+    return(
+        localStorage.getItem("user_id")?
+    <div className="search-page">
+        <br/>
+        <h1 className="subhead">Search Medications</h1>
+        <form className="d-flex search">
+            <input onChange={handleSearch} className="form-control me-2" type="search" placeholder="Search by Generic Name" aria-label="Search"/>
+            <button className="btn btn-outline-dark" type="submit">Search</button>
+        </form>
+        <ul>
+        {displayMeds.length>0?displayMeds.map((m)=>{return(<li className="med-li" key={m.id}>Generic Name: {m.generic_name}, Brand Name: {m.brand_name}, Dosage: {m.dosage} <a onClick={()=>setMed(m)} href={`/#/medications/${m.id}`} className="btn btn-outline-dark">Add Prescription</a></li>)}):null}
+        </ul>
+    </div>
+    : <LoginAuth user={user} setUser={setUser}/>
+    )
+}
+export default Search

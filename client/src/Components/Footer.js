cc2a1ff13cc6e2058e8563b00010b12da4127045
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 20:35:04 -0500

    home and footer

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+        
+    return(
+    
+    <footer className="page-footer font-small blue pt-4">   
+        <div className="footer-copyright text-center py-3">2021
+            <a href="https://github.com/kbergmanndean/"> https://github.com/kbergmanndean</a>
+        </div>
+    </footer>
+    )
+   }
+   export default Footer
---
+++ client/src/Components/Home.js
@@ -0,0 +1,20 @@
+import MyPre from "./MyPre"
+import bird2 from "../pictures/bird2.png"
+
+function Home({prescriptions, setPrescriptions}){
+
+    return(
+    <div className="home">
+        <div className="head">
+            <hr/>
+            <img src={bird2} style={{width:"100pt"}} alt="bird logo" className="bird"/>
+            <h1 className="header">MedReady</h1>
+            <hr/>
+        </div>
+        <h1 className="subhead">My Prescriptions</h1>
+        {localStorage.getItem("user_id") && prescriptions? <div className="card-holder row row-cols-1 row-cols-md-3">
+            {prescriptions.map((pre)=>{return <MyPre key={pre.id} pre={pre} prescriptions={prescriptions} setPrescriptions={setPrescriptions}/>})}</div>
+        : <><h2 className="center">Log in to see your prescriptions.</h2><br/><br/><br/><br/><br/><br/><br/><br/></>}
+    </div>
+    )}
+export default Home

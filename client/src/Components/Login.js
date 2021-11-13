7f844f725914579b0e79c9046b958126f0179023
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-12 20:35:34 -0500

    login

---
+++ client/src/Components/Login.js
@@ -0,0 +1,65 @@
+import {useHistory} from "react-router-dom"
+import {useState} from "react";
+
+function Login({setUser,user}){
+    const [username, setUserName]=useState('')
+    const [password, setPassword]=useState('')
+    const [errors,setErrors]=useState('')
+
+    const history=useHistory();
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        const user={
+            user_name:username,
+            password:password
+        }
+        const res=await fetch(`http://localhost:3000/log_in`,{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({user})
+        }); 
+        const userData=await res.json();
+        if (userData.id){
+            setUser(userData);
+            console.log(user);
+            localStorage.setItem("user_id", userData.id)
+            // history.push('/')
+        } else {
+            setErrors(userData.errors)
+            console.log(errors)
+        }
+    }
+    return(
+        <div>
+        <form onSubmit={handleSubmit}>
+            <br/>
+            <h1>Log In</h1>
+            <br/>
+            <input
+                type="text" 
+                placeholder="username"
+                value={username}
+                name="username"
+                onChange={(e)=>setUserName(e.target.value)}>
+            </input>
+            <input
+                type="text"
+                placeholder="password"
+                value={password}
+                name="password"
+                onChange={(e)=>setPassword(e.target.value)}>
+            </input>
+            <input type="submit" value="Log In" className="btn btn-outline-dark auth"></input>
+            <br/>
+            <br/>
+            {errors? errors.map(error=><div key={error}>{error}</div>):null}
+            {user? <p className="welcome">Welcome, {user.user_name} 
+            <br/>
+            <br/>
+             <a href="/" className="btn btn-outline-dark">Home</a></p>:null}
+        </form>
+        </div>
+    )
+}
+export default Login

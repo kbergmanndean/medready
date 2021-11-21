c39d18f29c8ac500225bcf897df49406072c4e39
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-21 15:20:29 -0500

    changed doctors to doctor in params require

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -28,7 +28,7 @@ class DoctorsController < ApplicationController
     private
 
     def doctor_params
-        params.require(:doctors).permit(:name, :profession, :user_id)
+        params.require(:doctor).permit(:name, :profession, :user_id)
     end
 
 

40447967eb64b592c22d0e73a54c65ee52885e1d
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-21 17:22:38 -0500

    added brackets to doctor controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,18 +2,18 @@ class DoctorsController < ApplicationController
     skip_before_action :authorize
     def index
         doctors=Doctor.all
-        render json: doctors, include: :user
+        render json: doctors, include: [:user]
     end
 
     def show
         doctor=Doctor.find_by(id:params[:id])
-        render json: doctor, include: :user
+        render json: doctor, include: [:user]
     end
 
     def create
         new_doctor=Doctor.create(doctor_params)
         if new_doctor.valid?
-            render json: new_doctor, include: :user, status: :created
+            render json: new_doctor, include: [:user], status: :created
         else
             render json: {error:new_doctor.errors.full_messages}, status: :unprocessable_entity
         end

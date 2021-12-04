57d8ac829f66b74c211c418c414754384f23f9c8
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-12-04 15:28:43 -0500

    added strftime formatting to script params in controller

--- client/src/Components/AddPre.js
+++ client/src/Components/AddPre.js
@@ -45,6 +45,7 @@ function AddPre({med, doctors, prescriptions, setPrescriptions, isMounted}){
             body:JSON.stringify(newPre)
         })
         const preAdded= await res.json();
+        console.log(preAdded)
         if (res.ok){
         addNewPre(preAdded);
         console.log(newPre);

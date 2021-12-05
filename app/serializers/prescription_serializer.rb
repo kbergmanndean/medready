6cbcff6a0215a6435faaa904b2edec115b82cbc4
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-12-04 20:24:18 -0500

    got rid of formatter in serializer

--- app/serializers/prescription_serializer.rb
+++ app/serializers/prescription_serializer.rb
@@ -1,5 +1,5 @@
 class PrescriptionSerializer < ActiveModel::Serializer
-  attributes :id, :daily_dosage, :date_given.strftime("%Y-%m-%d"), :directions, :doses_in_container, :doctor_id, :medication_id, :user_id
+  attributes :id, :daily_dosage, :date_given, :directions, :doses_in_container, :doctor_id, :medication_id, :user_id
   
   has_one :medication
   has_one :doctor

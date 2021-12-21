c6e3a3f1ae25db848e842013ed1cff44ce34dde0
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-12-21 13:48:46 -0500

    made form for adding meds

--- client/src/Components/AddMed.js
+++ client/src/Components/AddMed.js
@@ -1,6 +1,12 @@
-import React from "react"
+import React, {useState} from "react"
+
 
 function AddMed({user, setUser, meds, setMeds}){
+    const [brandName, setBrandName] = useState("")
+    const [genericName, setGenericName] = useState("")
+    const [dosage, setDosage] = useState("")
+    const [errors, setErrors] = useState([])
+
 
     const addNewMed = medAdded => {
         const newArray = [...meds, medAdded]
@@ -11,11 +17,39 @@ function AddMed({user, setUser, meds, setMeds}){
 
     async function handleSubmit(e){
         e.preventDefault();
+        const newMed={user_id: user_id, brand_name: brandName, generic_name: genericName, dosage: dosage}
+        const res=await fetch("https://medready.herokuapp.com/added_medications",{
+            headers:{"Content-Type":"application/json"},
+            method:"POST",
+            body:JSON.stringify(newMed)
+        });
      
     }
 
     return(
-        <p>Add Med</p>
+        <div className="addmed"> 
+        <div className="addmed-inner">
+        <br/>
+            <h1>Add Medication</h1>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="brand-name">Brand Name</label>
+                    <input type="text" className="form-control" id="brand-name" value={brandName} onChange={(e)=>setBrandName(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                <label htmlFor="medName">Generic Name</label>
+                <input type="text" className="form-control" id="medName" value={genericName} onChange={(e)=>setGenericName(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pill-dosage">Dosage</label>
+                    <input type="text" className="form-control" id="pill-dosage" value={dosage} onChange={(e)=>setDosage(e.target.value)}/>
+                </div>
+                    <br/>
+                    <button type="submit" className="btn btn-outline-dark form-submit">Submit</button>
+            </form>
+            {errors.length>=1? <ul style={{color:"red"}}>{errors.map((error)=>{return <li key={error}>{error}</li>})}</ul>: null}
+        </div>
+        </div>
     )
 }
 

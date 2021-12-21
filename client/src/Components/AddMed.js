a463ba326e4e06998fed888722904abc212cb693
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-12-21 12:54:39 -0500

    uncommented addmed stuff

--- client/src/Components/AddMed.js
+++ client/src/Components/AddMed.js
@@ -1,22 +1,22 @@
-// import React from "react"
+import React from "react"
 
-// function AddMed({user, setUser, meds, setMeds}){
+function AddMed({user, setUser, meds, setMeds}){
 
-//     const addNewMed = medAdded => {
-//         const newArray = [...meds, medAdded]
-//         setMeds(newArray);
-//     }
+    const addNewMed = medAdded => {
+        const newArray = [...meds, medAdded]
+        setMeds(newArray);
+    }
 
-//     const user_id=+localStorage.getItem("user_id")
+    const user_id=+localStorage.getItem("user_id")
 
-//     async function handleSubmit(e){
-//         e.preventDefault();
+    async function handleSubmit(e){
+        e.preventDefault();
      
-//     }
+    }
 
-//     return(
-//         <p>Add Med</p>
-//     )
-// }
+    return(
+        <p>Add Med</p>
+    )
+}
 
-// export default AddMed
+export default AddMed

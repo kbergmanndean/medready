00f81fcb62e0ea188e3cd61d19dd30090d749c0d
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-19 10:38:33 -0500

    changed home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -12,8 +12,9 @@ function Home({prescriptions, setPrescriptions}){
             <h1 className="header">MedReady</h1>
             <hr/>
         </div>
-        <h1 className="subhead">My Prescriptions</h1>
+        
         {localStorage.getItem("user_id") && prescriptions? <div className="card-holder row row-cols-1 row-cols-md-3">
+            <h1 className="subhead">My Prescriptions</h1>
             {prescriptions.map((pre)=>{return <MyPre key={pre.id} pre={pre} prescriptions={prescriptions} setPrescriptions={setPrescriptions}/>})}</div>
         : <><h2 className="center">Log in to see your prescriptions.</h2><br/><br/><br/><br/><br/><br/><br/><br/></>}
     </div>
